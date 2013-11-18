4556770566e53841bdc99a2a697a4d8c6f6c5342
Fightbackman <fightbackman@aol.com>
2013-11-19 00:04:10 +0100

    Added second abs methode

--- Hausaufgabe3/src/h6/betrag.java
+++ Hausaufgabe3/src/h6/betrag.java
@@ -8,7 +8,16 @@ public class betrag {
 
 	}
 
-	public void absolutwert(int x_) {
+	public void absolutwert(int x_) { //effizientere Methode
 		x = Math.abs(x_);
+	}
+	
+	public int absolutwert2(int x_){ //einfacher zu beweisende Methode
+		if(x_>=0){
+			return x_;
+		}
+		else {
+			return -(x_);
+		}
 	}
 }
