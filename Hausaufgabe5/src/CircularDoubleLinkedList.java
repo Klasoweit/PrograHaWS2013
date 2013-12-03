5bf703aef0dd7f7a4df6d4657cfaffb9aae380c9
Fightbackman <fightbackman@aol.com>
2013-12-03 11:44:04 +0100

    cleaned up, finished Hausuafgabe5

--- Hausaufgabe5/src/CircularDoubleLinkedList.java
+++ Hausaufgabe5/src/CircularDoubleLinkedList.java
@@ -1,7 +1,24 @@
 class CircularDoubleLinkedList {
 	public CircularDoubleLinkedList previous, next;
 	public int value;
-
+//main methode zum testen
+/*	public static void main(String[] args){
+		CircularDoubleLinkedList test = new CircularDoubleLinkedList(10);
+		test.testen(test);
+		test.insert(10);
+		test.insert(9);
+		test.insert(8);
+		test.insert(7);
+		test.insert(6);
+		test.insert(5);
+		test.insert(4);
+		test.insert(3);
+		test.insert(2);
+		test.insert(1);
+		test.insert(11);
+	}
+*/
+	
 	public CircularDoubleLinkedList(int value) {
 		this.value = value;
 		this.previous = this;
@@ -14,48 +31,40 @@ class CircularDoubleLinkedList {
 		this.value = value;
 		this.previous = previous;
 		this.next = next;
+		this.next.previous = this;
+		this.previous.next = this;
+		assert (this.checkStructure());
 	}
 
-	/*
-	 * Gegeben ist der Rump einer zirkulaer doppelt-verketteten Liste. In so
-	 * einer Liste kann von jedem Elemtn auf das vorherige und dsa nachfoglende
-	 * Element der Liste zugegriffen werden. Die Elemente formen einen Kreis,
-	 * die Liste hat also keinen Anfang oder Ende.
-	 *  1. Implementieren Sie die
-	 * Methode public int size(), welhe die Anzahl an Elementen in der Liste
-	 * zurueckgibt.
-	 *  2. Implementieren Sie die Methode public
-	 * CircualrDoubleLinkedList insert(int value). Die Methode soll einen neues
-	 * Element zur Liste hinzufuegen und eine Referenz auf dieses Element
-	 * zurueckgeben.
-	 *  3. Implementieren Sie die Methode public boolean
-	 * checkStructure() um folgende Invariante der Liste zu ueberpruefen: ein
-	 * Listenelement hat ein Vorgaengerlement previous und ein Nachfolgerelement
-	 * next, beide duerfen nicht null sein. Weiterhi gilt fuer jedes
-	 * Listenelement, dass der Vorgaenger des Nachfolgers das Element selbst ist;
-	 * genauso verhaelt es sich mit dem Nachfolger des Vorgaengers. Die Methode
-	 * checkStructure() soll diese Invariante fuer das Element, fuer welches sie
-	 * aufgerufen wird, ueberpruefen und genau dann true zurueckgeben, wenn diese
-	 * Invariante gegeben ist.
-	 * Rufen sie checkStructure() in einem assert vor und
-	 * nach dem Ausfuehren jeder public Methide fuer jedes geaenderte Element sowie
-	 * jedes Element, dass eine Referent auf ein geaendertes Element besitzt,
-	 * auf. Pruefen Sie mit einem solchen assert auch, ob die Invariante am Ende
-	 * jedes Konstruktors fuer das gerade erzeugte Element gilt.
-	 * (a) Stellt diese Invariante sicher, dass die Liste immer kreisfoermig ist?
-	 * (b) Stellt diese Invariante sicher, dass die Liste, welche von insert
-	 * 	   zurueckgegeben wird, auch alle Elemente der urspruenglichen Liste beinhaltet?
-	 */
-
 	public CircularDoubleLinkedList insert(int value) {
-		// TODO
+		assert (this.checkStructure());
+		assert (this.next.checkStructure());
+		CircularDoubleLinkedList elem = new CircularDoubleLinkedList(value,
+				this, this.next);
+		assert (this.checkStructure());
+		assert (this.next.checkStructure());
+		return elem;
+
 	}
 
 	public int size() {
-		// TODO
+		CircularDoubleLinkedList temp = this;
+		int anzahl = 1;
+		while (temp.next != this) {
+			anzahl++;
+			temp = temp.next;
+		}
+		return anzahl;
 	}
 
 	public boolean checkStructure() {
-		// TODO
+		try {
+			if (this.previous.next != this | this.next.previous != this) {
+				return false;
+			}
+		} catch (NullPointerException e) {
+			return false;
+		}
+		return true;
 	}
 }
